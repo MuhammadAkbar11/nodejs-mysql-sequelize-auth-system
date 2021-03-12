b21ec20f1bbcb52bce5bd6fb4b0fdb2f96fe0fa8
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-12 15:03:54 +0700

    logout and login routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth");
+const signupSchema = require("../middleware/schema/signupSchema");
+const loginSchema = require("../middleware/schema/loginShema");
+const checkIsLogin = require("../middleware/checkIsLogin");
 
-router.get("/signup", authController.getSignup);
+router.get("/signup", [checkIsLogin], authController.getSignup);
+router.post("/signup", [signupSchema], authController.postSingUp);
+router.get("/login", [checkIsLogin], authController.getLogin);
+router.post("/login", [loginSchema], authController.postLogin);
+router.post("/logout", authController.logout);
 
 module.exports = router;
